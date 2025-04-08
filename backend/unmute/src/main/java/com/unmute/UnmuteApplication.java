938aa39f6e8aa78aec3b47c78471d0864a370e1e
Chandan Jaladi <chandanj4334@gmail.com>
2025-04-08 00:25:45 -0400

    Added spring boot backend and organized frontend structure

---
+++ backend/unmute/src/main/java/com/unmute/UnmuteApplication.java
@@ -0,0 +1,13 @@
+package com.unmute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnmuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnmuteApplication.class, args);
+	}
+
+}
---
+++ backend/unmute/src/test/java/com/unmute/UnmuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unmute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnmuteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
